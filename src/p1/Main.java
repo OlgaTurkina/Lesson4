a73e0e91f88eb4a7a0d161b62f4829e9580a420d
OlgaTurkina <olechka967@rambler.ru>
2023-10-12 10:36:51 +0400

    1

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,1 +1,15 @@
-P
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        boolean c = a == b; // false
+        boolean d = a == 10; // true
+        boolean e = a != b; // true
+        boolean h = a != 10; // false
+        boolean f = a < 10; // false
+        boolean g = a < b; // false
+        boolean n = a > b; // true
+        boolean m = a <= 10; // true
+        boolean k = a >= b; // true
+    }
+}
