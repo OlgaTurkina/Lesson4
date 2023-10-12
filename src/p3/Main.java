6d4adca7b9a19a30235c0fecc2123273e5db207c
OlgaTurkina <olechka967@rambler.ru>
2023-10-12 11:56:09 +0400

    3

--- src/p3/Main.java
+++ src/p3/Main.java
@@ -10,6 +10,37 @@ public class Main {
         //c*=b; // c = c * b
         //c/=b; // c = c / b
         //c%=b; // c = c / b * 100
-    System.out.println(c);
+        //System.out.println(c);
+
+        int num1 = 4;
+        int num2 = 5;
+
+        if (num1>num2){
+            System.out.println("First > second");
+        }else if (num1<num2){
+            System.out.println("First < second");
+        }else {
+            System.out.println("First == second");
+        }
+
+        int num = 7;
+
+        switch (num){
+            case 1:
+                System.out.println("Number = 1");
+                break;
+            case 8:
+                System.out.println("Number = 8");
+                break;
+            case 9:
+                System.out.println("Number = 9");
+                break;
+            default:
+                System.out.println("Number != 1, 8, 9");
+                break;
+        }
+
+
+
     }
 }
