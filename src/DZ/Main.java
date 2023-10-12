617b716e698ecc8a2c833fa61c3ce35f534c0161
OlgaTurkina <olechka967@rambler.ru>
2023-10-12 12:50:05 +0400

    DZ

---
+++ src/DZ/Main.java
@@ -0,0 +1,53 @@
+package DZ;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! It`s a calc");
+        Scanner calc = new Scanner(System.in);
+        System.out.println("Input x - first number");
+        int x = calc.nextInt();
+        System.out.println("Input y - second number");
+        int y = calc.nextInt();
+        System.out.println("Input ~1~, if +, ~2~, if -, ~3~, if *, ~3~, if /");
+        int j = calc.nextInt();
+        System.out.printf("x = %d, y = %d, j = %d \n", x, y, j);
+
+        int a = x+y;
+        int b = x-y;
+        int c = x*y;
+        double d = (double)x / y;
+
+        switch (j){
+            case 1:
+                System.out.printf("x+y = %d \n", a);
+                break;
+            case 2:
+                System.out.printf("x-y = %d \n", b);
+                break;
+            case 3:
+                System.out.printf("x*y = %d \n", c);
+                break;
+            case 4:
+                System.out.printf("x/y = %.2f \n", d);
+                break;
+            default:
+                System.out.printf("Error \n");
+        }
+
+        /*if (j == 1){
+            System.out.printf("x+y = %d \n", a);
+        }else if (j == 2){
+            System.out.printf("x-y = %d \n", b);
+        }else if (j == 3){
+            System.out.printf("x*y = %d \n", c);
+        }else if (j == 4){
+            System.out.printf("x/y = %.2f \n", d);
+       }else {
+            System.out.printf("Error \n");
+        }*/
+        System.out.println("Bye");
+        calc.close();
+    }
+}
